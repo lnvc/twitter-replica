0c9d7978634c5f3d3f43e44709c5970464b12674
Laira <laira.cham@gmail.com>
2020-07-25 11:17:27 +0800

    first

---
+++ resources/js/components/Tweet.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container-fluid px-0 py-0">
+        <form action="/tweet" enctype="multipart/form-data" method="POST" class="border-bottom">
+            <input type="hidden" name="_token" :value="csrf">
+            <div class="row container-fluid py-2">
+                <div class="col col-md-auto px-2 pt-2">
+                    <a :href="profile.handle"><img :src="'../../../storage/pfp/' + profile.profile_pic" class="rounded-circle" style="height:50px;width:50px" alt=""></a>
+                </div>
+                <div class="form-group col col-md-auto px-2" style="width: 85%;">
+                    <input type="text" v-model="tweet" id="tweet" name="tweet" style="border: transparent; outline: none; width: 100%;" placeholder="What's happening?" v-on:keyup="countChars">
+                </div>
+            </div>
+            <div class="text-right px-2 py-2">
+                <button type="submit" class="btn btn-primary rounded">Tweet</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Tweet",
+        props: ["profile"],
+        methods: {
+            countChars() {
+                if(this.tweet.length <= 250){
+                    console.log(this.tweet.length);
+                }
+                else{
+
+                }
+            }
+        },
+        data() {
+            return {
+                csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+            }
+        }
+    }
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,10 @@
 
 @section('content')
 <div class="container-fluid px-0">
-  <form action="/tweet" enctype="multipart/form-data" method="POST" class="border-bottom">
-    @csrf
-    <h4 class="pl-2 pb-2 border-bottom"><b>Home</b></h4>
-    <div class="row container-fluid py-2">
-    <div class="col col-md-auto px-2 pt-2">
-        <a href="{{ '/' . $profile->handle }}"><img src="{{ asset('storage/pfp/'. $profile->profile_pic) }}" class="rounded-circle" style="height:50px;width:50px" alt=""></a>
-    </div>
-    <div class="form-group col col-md-auto px-2" style="width: 85%;">
-        <input type="text" style="border: transparent; outline: none; width: 100%;" placeholder="What's happening?">
-    </div>
-    </div>
-    <div class="text-right px-2 py-2">
-    <button type="submit" class="btn btn-primary rounded">Tweet</button>
-    </div>
-
-  </form>
+  <h4 class="pl-2 pb-2 border-bottom"><b>Home</b></h4>
+  <div class="make-tweet">
+    <Tweet :profile="{{$profile}}" />
+  </div>
   @if ($tweets)
     <div>
       @each('partials.tweet', $tweets, 'tweet')
